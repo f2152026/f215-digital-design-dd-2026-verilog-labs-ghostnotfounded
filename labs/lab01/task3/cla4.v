f13695e5b113976cdfc06bc88b7377411900fc20
Arsheya Singh Parmar <f20230304@pilani.bits-pilani.ac.in>
2026-08-29 16:08:46 +0000

    [task3] 4-bit Carry Look Ahead Adder GATE LEVEL

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -33,5 +33,21 @@ module cla4(
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
+  assign p0 = a[0] ^ b[0];
+  assign p1 = a[1] ^ b[1];
+  assign p2 = a[2] ^ b[2];
+  assign p3 = a[3] ^ b[3];
+
+  assign g0 = a[0] & b[0];
+  assign g1 = a[1] & b[1];
+  assign g2 = a[2] & b[2];
+  assign g3 = a[3] & b[3];
+
+  assign c1   = (p0 & cin) | g0;
+  assign c2   = (p1 & p0 & cin) | (p1 & g0) | g1;
+  assign c3   = (p2 & p1 & p0 & cin) | (p2 & p1 & g0) | (p2 & g1) | g2;
+  assign cout = (p3 & p2 & p1 & p0 & cin) | (p3 & p2 & p1 & g0) | (p3 & p2 & g1) | (p3 & g2) | g3;
+
+  assign sum = { p3 ^ c3, p2 ^ c2, p1 ^ c1, p0 ^ cin };
 
 endmodule
